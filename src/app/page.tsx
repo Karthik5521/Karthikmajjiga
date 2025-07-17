a1a61973158dae2931d07e698a5a8052f62c2b4f
Karthik5521 <majjigakarthik5@gmail.com>
2025-07-17 11:23:17 -0500

    update counts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Experience from "@/sections/Experience";
 import Hero from "@/sections/Hero";
 import Projects from "@/sections/Projects";
 import Skills from "@/sections/Skills";
+import { Analytics } from "@vercel/analytics/next"
 
 
 export default function Home() {
